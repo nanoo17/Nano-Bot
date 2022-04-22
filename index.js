5acbc93133d67491bc11ead4941394f0a1257e79
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-04-22 19:21:17 -0300

    agregado de comandos

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const Discord = require("discord.js");
+//definiendo permisos del bot
+const intents = new Discord.Intents(131071);
+//creando cliente
+const client = new Discord.Client({ intents });
 
 client.on('ready', () => {
-  console.log(`Bot logueado! ${client.user.tag}`);
+  console.log(`Bot Logueado! ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async msg => {
-  // if (msg.content === 'ping') {
-  //   msg.reply('Pingo!');
-  // }
-  console.log(msg.content);
+client.on('messageCreate', async (message) => {
+  //el bot no responderá mensajes privados
+  if (message.channel.type === 'DM') return;
+  //el bot no respondera mensajes de otros bots
+  if (message.author.bot) return;
+  //definimos el prefijo de los comandos
+  let prefix = "!";
+  //condicionando que los mensajes comiencen con el prefijo
+  if (!message.content.startsWith(prefix)) return;
+  //definiendo argumentos separados por espacios
+  const args = message.content.slice(prefix.length).trim().split(/ +/g); // Definimos args. Explicado en el video
+  //definiendo comandos, serán las palabras siguientes al prefijo sin ningun espacio
+  const command = args.shift().toLowerCase() // Definimos command, tambien explicado en el video
+  //comando ping
+  if (command === "ping") {
+    message.reply("Pingo!")
+  }
+  if (command === "hola") {
+    message.reply("Hola que tal!")
+  }
 });
 
-client.login('OTY3MTA4NDc4MzQwNTkxNjQ3.YmLf9Q.QRrgjGp1EEy6O1EejuX-raLfb_w');
+//borrar token al commitear
+client.login('OTY3MTA4NDc4MzQwNTkxNjQ3.YmLf9Q.UZsIgSafKTnUWXwCfTaL3LLq_1o');
